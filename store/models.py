67b8b411a64767999aa0d80b3bc0b05c2900d977
Zahra Reyhanian <reyhanyanz@gmail.com>
2022-07-22 16:15:30 +0430

    add validator for img and manage img in admin

--- store/models.py
+++ store/models.py
@@ -1,8 +1,11 @@
+from wsgiref.validate import validator
 from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
+
+from store.validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -44,7 +47,9 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(
+                upload_to='store/images',
+                validator=[validate_file_size])
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
